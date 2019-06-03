2810de7c8302278a236127c5f8f8f70789b662c6
Paulo Côto <pbarca@hotmail.com>
2019-06-03 16:22:32 +0100

    base

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flas, render
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('registo.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
